25506a0fda9f8c7693108259188dfe4fdd8abf60
weekend <weekend@program>
2021-08-03 21:08:49 +0300

    i revert "i use deps-repl library for repl"
    This reverts commit 0108cc36889f567c1c085fccc88ebd0947d438a8.

--- program.sh
+++ program.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 repl(){
-  clj \
-    -X:repl deps-repl.core/process \
-    :main-ns weekend.main \
-    :port 7788 \
-    :host '"0.0.0.0"' \
-    :repl? true \
-    :nrepl? false
+  clj -A:rpel
 }
 
 "$@"
