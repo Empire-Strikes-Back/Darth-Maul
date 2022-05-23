d38b9f1bb7645de57746e543a79ae596c78cb17b
Darth-Maul <Darth-Maul@pirouette>
2022-05-23 18:33:32 +0300

    i am full jvm dekstop program - these clothes are heavy and idenicon is idiotic at last we will have our revenge

--- program.sh
+++ program.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
 repl(){
-  clj -A:rpel
+  clj \
+    -J-Dclojure.core.async.pool-size=1 \
+    -X:Ripley Ripley.core/process \
+    :main-ns Darth-Maul.main
+}
+
+
+main(){
+  clojure \
+    -J-Dclojure.core.async.pool-size=1 \
+    -M -m Darth-Maul.main
+}
+
+tag(){
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  TAG="$COMMIT_COUNT-$COMMIT_HASH"
+  git tag $TAG $COMMIT_HASH
+  echo $COMMIT_HASH
+  echo $TAG
+}
+
+jar(){
+
+  clojure \
+    -X:Zazu Zazu.core/process \
+    :word '"Darth-Maul"' \
+    :filename '"out/identicon/icon.png"' \
+    :size 256
+
+  rm -rf out/*.jar
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  clojure \
+    -X:Genie Genie.core/process \
+    :main-ns Darth-Maul.main \
+    :filename "\"out/Darth-Maul-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :paths '["src" "out/identicon"]'
+}
+
+release(){
+  jar
 }
 
 "$@"
---
+++ src/Darth_Maul/peanuts.clj
@@ -0,0 +1,14 @@
+(ns Darth-Maul.peanuts
+  (:require
+   [clojure.core.async :as Little-Rock
+    :refer [chan put! take! close! offer! to-chan! timeout thread
+            sliding-buffer dropping-buffer
+            go >! <! alt! alts! do-alts
+            mult tap untap pub sub unsub mix unmix admix
+            pipe pipeline pipeline-async]]
+   [clojure.java.io :as Wichita.java.io]
+   [clojure.string :as Wichita.string]
+
+   [Darth-Maul.seed]))
+
+(do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
